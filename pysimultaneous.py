c34f2fad0ddc7e481314f93e357e1d4cb6806b13
a676-code <109701008+thisusernameisaparadox@users.noreply.github.com>
2024-03-12 11:09:08 -0400

    created Player class; improved simGame class

--- pysimultaneous.py
+++ pysimultaneous.py
@@ -3,15 +3,30 @@
 # Date: 3/10/24
 # Description: a class for handling simultaneous games with n players, n >= 2
 
+class Player:
+    numStrats = 2
+    
+    def __init__(self,numStrats = 2):
+        self.numStrats = numStrats
+
 class simGame:
     numPlayers = 2
     numOutcomes = 4
-    payoffMatrix
+    payoffMatrix = []
     
-    mixedEquilibria
-    paretoPureEquilibria
-    pureEquilibria
+    players = []
     
-    def __init__(self, numPlayers=2):
+    mixedEquilibria = []
+    paretoPureEquilibria = []
+    pureEquilibria = []
+    
+    def __init__(self, numPlayers=2, numStrats = [2, 2]):
+        for i in range(numPlayers):
+            p = Player(numStrats[i])
+            self.players.append(p)
+        
         self.numPlayers = numPlayers
+        self.payoffMatrix = []
         return
+    
+G = simGame(2, [2, 2])
