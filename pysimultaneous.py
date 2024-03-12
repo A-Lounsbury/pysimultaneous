c42054ad17bb9ccaf41ad79b4c9181b3f1d599e1
a676-code <109701008+thisusernameisaparadox@users.noreply.github.com>
2024-03-12 18:40:58 -0400

    added enterPayoffs function

--- pysimultaneous.py
+++ pysimultaneous.py
@@ -4,23 +4,20 @@
 # Description: a class for handling simultaneous games with n players, n >= 2
 
 class Player:
-    numStrats = 2
+    numStrats = -1
     
     def __init__(self,numStrats = 2):
         self.numStrats = numStrats
 
 class simGame:
-    numPlayers = 2
-    numOutcomes = 4
+    numPlayers = -1
     payoffMatrix = []
-    
     players = []
     
     mixedEquilibria = []
-    paretoPureEquilibria = []
     pureEquilibria = []
     
-    def __init__(self, numPlayers=2, numStrats = [2, 2]):
+    def __init__(self, numPlayers = 2, numStrats = [2, 2]):
         for i in range(numPlayers):
             p = Player(numStrats[i])
             self.players.append(p)
@@ -35,13 +32,24 @@ class simGame:
                 
         return
     
+    def enterPayoffs(self, payoffs = [[[0, 0], [0, 0]], [[0, 0], [0, 0]]]):
+        self.payoffMatrix = payoffs
+    
     def printGame(self):
         for i in range(self.players[0].numStrats):
             for j in range(self.players[1].numStrats):
                 for n in range(self.numPlayers):
-                    print(self.payoffMatrix[i][j][n], end="")
+                    if n < self.numPlayers - 1:
+                        print(self.payoffMatrix[i][j][n], end=", ")
+                    else:
+                        print(self.payoffMatrix[i][j][n], end="")
                 print(" ", end="")
             print()
             
 G = simGame(2, [2, 2])
+G.printGame()
+
+newPayoffs = [[[-3, -3], [0, -5]], [[-5, 0], [-1, -1]]]
+
+G.enterPayoffs(newPayoffs)
 G.printGame()
