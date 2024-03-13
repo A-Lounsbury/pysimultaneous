906b85f41d840886f57e1c57df9c03a45015c4d0
a676-code <109701008+thisusernameisaparadox@users.noreply.github.com>
2024-03-12 22:53:43 -0400

    fixed printGame function

--- pysimultaneous.py
+++ pysimultaneous.py
@@ -4,7 +4,13 @@
 # Description: a class for handling simultaneous games with n players, n >= 2
 
 class Node:
+    head = None
+    payoff = -1
+    bestResponse = True
+    next = None
+    
     def __init__(self, payoff, bestResponse):
+        self.head = self
         self.payoff = 0
         self.bestResponse = False
         self.next = None
@@ -20,6 +26,27 @@ class Node:
             curNode = curNode.next
         
         curNode.next = newNode
+        
+    def getNode(self, index):
+        if self.head == None:
+            return
+ 
+        curNode = self.head
+        pos = 0
+        if pos == index:
+            return curNode
+        else:
+            while(curNode != None and pos + 1 != index):
+                pos = pos + 1
+                curNode = curNode.next
+ 
+            if curNode != None:
+                return curNode
+            else:
+                print("Index not present")
+                
+    def getPayoff(self):
+        return self.payoff
 
     def insertAtBeginning(self, payoff, bestResponse):
         newNode = Node(payoff, bestResponse)
@@ -59,12 +86,18 @@ class Node:
                 
     def printLL(self):
         curNode = self.head
+        size = self.sizeOfLL()
+        i = 0
         while(curNode):
-            print(curNode.data)
+            if i < size - 1:
+                print(curNode.payoff, end=", ")
+            else:
+                print(curNode.payoff, end=" ")
             curNode = curNode.next
+            i += 1
             
-    def printNode(self):
-        print(self.payoff)
+    def printNode(self, end=""):
+        print(self.payoff, end=end)
     
     def removeAtIndex(self, index):
         if self.head == None:
@@ -140,9 +173,8 @@ class simGame:
             for i in range(self.players[0].numStrats):
                 row = []
                 for j in range(self.players[1].numStrats):
-                    outcome = []
-                    for x in self.players:
-                        outcome.append(0)
+                    outcome = Node(0, True)
+                    outcome.append(0, True)
                     row.append(outcome)                        
                 self.payoffMatrix.append(row)
         else:
@@ -154,13 +186,12 @@ class simGame:
                 for i in range(self.players[0].numStrats):
                     row = []
                     for j in range(self.players[1].numStrats):
-                        outcome = []
+                        outcome = Node(0, True)
                         for x in self.players:
-                            outcome.append(0)
+                            outcome.append(0, True)
                         row.append(outcome)                 
                     matrix.append(row)
                 self.payoffMatrix.append(matrix)
-                
         return
     
     def enterPayoffs(self, payoffs = [[[0, 0], [0, 0]], [[0, 0], [0, 0]]]):
@@ -176,15 +207,24 @@ class simGame:
         p1BR = True
         p2BR = True
         
-        for i in range(self.players[0].numStrats):
-            if self.payoffMatrix[p1Strat][p2Strat][0] < self.payoffMatrix[i][p2Strat][0]:
-                p1BR = False
-        
-        for j in range(self.players[1].numStrats):
-            if self.payoffMatrix[p1Strat][p2Strat][1] < self.payoffMatrix[p1Strat][j][1]:
-                p2BR = False
-        return (p1BR, p2BR)
-        
+        if self.numPlayers < 3:
+            for i in range(self.players[0].numStrats):
+                if self.payoffMatrix[p1Strat][p2Strat].getNode(0).payoff < self.payoffMatrix[i][p2Strat].getNode(0).payoff:
+                    p1BR = False
+            
+            for j in range(self.players[1].numStrats):
+                if self.payoffMatrix[p1Strat][p2Strat].getNode(1).payoff < self.payoffMatrix[p1Strat][j].getNode(1).payoff:
+                    p2BR = False
+            return (p1BR, p2BR)
+        else:
+            for m in range(len(self.payoffMatrix)):
+                for i in range(self.players[0].numStrats):
+                    if self.payoffMatrix[p1Strat][p2Strat].getNode(0).payoff < self.payoffMatrix[i][p2Strat].getNode(0).payoff:
+                        p1BR = False
+                
+                for j in range(self.players[1].numStrats):
+                    if self.payoffMatrix[p1Strat][p2Strat].getNode(1).payoff < self.payoffMatrix[p1Strat][j].getNode[1].payoff:
+                        p2BR = False
     
     def printGame(self):
         """Prints the payoff matrix
@@ -192,59 +232,53 @@ class simGame:
         if self.numPlayers < 3:
             for i in range(self.players[0].numStrats):
                 for j in range(self.players[1].numStrats):
-                    for n in range(self.numPlayers):
-                        if n < self.numPlayers - 1:
-                            print(self.payoffMatrix[i][j][n], end=", ")
-                        else:
-                            print(self.payoffMatrix[i][j][n], end="")
-                    print(" ", end="")
-                print()
+                    self.payoffMatrix[i][j].printLL()
+                    if j == self.players[1].numStrats - 1:
+                        print()
+            print()
         else:
             for m in range(len(self.payoffMatrix)):
                 for i in range(self.players[0].numStrats):
                     for j in range(self.players[1].numStrats):
-                        for n in range(self.numPlayers):
-                            if n < self.numPlayers - 1:
-                                print(self.payoffMatrix[m][i][j][n], end=", ")
-                            else:
-                                print(self.payoffMatrix[m][i][j][n], end="")
-                        print(" ", end="")
-                    print()
+                        self.payoffMatrix[m][i][j].printLL()
+                        if j == self.players[1].numStrats - 1:
+                            print()
                 print()
             
-G = simGame(3)
+G = simGame(2)
+print("G:",)
 G.printGame()
 
 # newPayoffs = [[[-3, -3], [0, -5]], [[-5, 0], [-1, -1]]]
 
 # G.enterPayoffs(newPayoffs)
-G.printGame()
+# G.printGame()
 
-strat1 = 0
-strat2 = 0
-result = G.isBestResponse(strat1, strat2)
+# strat1 = 0
+# strat2 = 0
+# result = G.isBestResponse(strat1, strat2)
 
-if result[0]:
-    print("YES")
-else:
-    print("NO")
+# if result[0]:
+#     print("YES")
+# else:
+#     print("NO")
     
-if result[1]:
-    print("YES")
-else:
-    print("NO")
+# if result[1]:
+#     print("YES")
+# else:
+#     print("NO")
     
-print()
-strat1 = 1
-strat2 = 1
-result = G.isBestResponse(strat1, strat2)
+# print()
+# strat1 = 1
+# strat2 = 1
+# result = G.isBestResponse(strat1, strat2)
 
-if result[0]:
-    print("YES")
-else:
-    print("NO")
+# if result[0]:
+#     print("YES")
+# else:
+#     print("NO")
     
-if result[1]:
-    print("YES")
-else:
-    print("NO")
+# if result[1]:
+#     print("YES")
+# else:
+#     print("NO")
