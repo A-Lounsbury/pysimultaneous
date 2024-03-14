127bca45d294b4bbaca27e3089b4f89176c777d6
a676-code <109701008+thisusernameisaparadox@users.noreply.github.com>
2024-03-13 23:33:26 -0400

    added hash and unhash functions

--- pysimultaneous.py
+++ pysimultaneous.py
@@ -165,12 +165,15 @@ class simGame:
     mixedEquilibria = []
     pureEquilibria = []
     
+    impartial = False
+    
     def __init__(self, numPlayers = 2):
         numStrats = [2 for i in range(numPlayers)]
         self.players = [Player(numStrats[i]) for i in range(numPlayers)]
         
         self.numPlayers = numPlayers
         self.payoffMatrix = []
+        self.impartial = False
         if self.numPlayers < 3:
             for i in range(self.players[0].numStrats):
                 row = []
@@ -179,6 +182,7 @@ class simGame:
                     outcome.append(0, True)
                     row.append(outcome)                        
                 self.payoffMatrix.append(row)
+                
         else:
             numMatrices = 1
             for i in range(3, self.numPlayers):
@@ -195,6 +199,16 @@ class simGame:
                     matrix.append(row)
                 self.payoffMatrix.append(matrix)
         return
+    
+    def computeImpartiality(self):
+        """ ? 
+        """
+        num = self.players[0].numStrats
+        for x in range(1, self.numPlayers):
+            if self.players[x].numStrats != num:
+                self.impartial = False
+                return
+        impartial = True
     
     def enterPayoffs(self, payoffs = [[[0, 0], [0, 0]], [[0, 0], [0, 0]]]):
         self.payoffMatrix = payoffs
@@ -227,6 +241,37 @@ class simGame:
                 for j in range(self.players[1].numStrats):
                     if self.payoffMatrix[p1Strat][p2Strat].getListNode(1).payoff < self.payoffMatrix[p1Strat][j].getNode[1].payoff:
                         p2BR = False
+                        
+    def hash(self, profile):
+        """Converts a sequence of strategies into the index in a stack of payoff arrays that correspond to that sequence
+
+        Args:
+            profile (list): strategy profile (indices)
+
+        Returns:
+            int: the desired index
+        """
+        self.computeImpartiality() # ? 
+        
+        # c_2 + sum_{x = 3}^{nP - 1} (nS)^x * c_x
+        num = 0 # return 0 if numPlayers < 2
+        if self.numPlayers > 2:
+            num = profile[2]
+        if self.impartial:
+            for x in range(3, self.numPlayers):
+                if profile.at(x) > 0:
+                    num += self.players[0]**(x - 2) * self.profile[x]
+        else: # c_2 + sum_{x=3}^{nP} nS_2 *...* nS_{x-1} * c_x
+            if self.numPlayers > 3:
+                product = 0
+                for x in range(3, numPlayers):
+                    product = 1
+                    if profile[x] > 0:
+                        for y in range(2, x - 1):
+                            product *= self.players[y].numStrats
+                            
+                        num += product * profile[x]
+        return num
     
     def printGame(self):
         """Prints the payoff matrix
@@ -247,10 +292,64 @@ class simGame:
                             print()
                         print()
                 print()
+                
+    def removeStrategy(x, s):
+        """Removes strategy s from player x in the payoff matrix
+
+        Args:
+            x (int): index of the player
+            s (int): index of the strategy
+        """
+        if x == 0:
+            for m in range(len(self.payoffMatrix)):
+                return
+        if x == 1:
+            for m in range(len(self.payoffMatrix)):
+                return
+        else:
+            numErased = 0
+            product = 1
+            m = 0
+     
+    def unhash(self, m):
+        """Converts an index in a stack of payoff arrays into the sequence of strategies that produce that index
+
+        Args:
+            m (int): the index of the payoff array that we're unhashing
+
+        Returns:
+            list: a list of indices (strategies)
+        """
+        choice = 0
+        prevValues = 0 # values from players below P_x
+        productNumStrats = 1
+        profile = [0 for i in range(self.numPlayers)]
+        profile[0] = -1
+        profile[1] = -1
+        
+        for x in range(2, self.numPlayers - 1):
+            productNumStrats *= self.players[x].numStrats
             
+        for x in range(self.numPlayers - 1, 1, -1):
+            choice = 0
+            while productNumStrats * choice + prevValues < m and choice != self.players[x].numStrats - 1:
+                choice += 1
+            
+            if productNumStrats * choice + prevValues > m:
+                choice -= 1
+                
+            prevValues += productNumStrats * choice
+            profile[x] = choice
+            productNumStrats = productNumStrats / self.players[x].numStrats
+        return profile
+
 G = simGame(3)
 print("G:")
 G.printGame()
+
+print("hash:", G.hash([1, 1, 0]))
+
+print("unhash:", G.unhash(0))
 
 # newPayoffs = [[[-3, -3], [0, -5]], [[-5, 0], [-1, -1]]]
 
